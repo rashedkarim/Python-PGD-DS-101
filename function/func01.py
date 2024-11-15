b394cf3dc299f05641e5c739e0983f969be5b39d
rashedkarim <mail4rashed@gmail.com>
2024-11-15 14:59:02 +0600

    func01

--- function/func01.py
+++ function/func01.py
@@ -2,7 +2,6 @@ def my_function(**kid):
    print(kid)
    print("His last name is " + kid["lname"])
 
-
 d = {"fname": "Tobias", "lname": "Refsnes"}
 
 my_function(fname = "Tobias", lname = "Refsnes")
